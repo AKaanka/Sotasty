b29e27bdc3b92b2cedc3e6fa1af478015a8471b0
AKaanka <kaan.kadioglu@code.berlin>
2025-09-13 15:33:16 +0200

    Update RecipeFactory.php

--- database/factories/RecipeFactory.php
+++ database/factories/RecipeFactory.php
@@ -2,22 +2,21 @@
 
 namespace Database\Factories;
 
+use App\Models\Recipe;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Recipe>
- */
 class RecipeFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Recipe::class;
+
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(),
+            'content' => fake()->paragraphs(3, true),
+            'published' => fake()->boolean(),
+            'author_id' => fake()->numberBetween(1,10),
         ];
     }
 }
