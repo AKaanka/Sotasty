144a0b482c8db96f8a75fbc07116d37cc1b84762
AKaanka <kaan.kadioglu@code.berlin>
2025-10-06 18:15:29 +0200

    FEATURE: migration table is reorganized

--- database/migrations/2025_10_06_150618_create_recipes_table.php
+++ database/migrations/2025_10_06_150618_create_recipes_table.php
@@ -11,12 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('recipes', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+         Schema::create('recipes', function (Illuminate\Database\Schema\Blueprint $table) {
+        $table->id();
+
+        // these TWO lines are crucial
+        $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+        $table->foreignId('cat_id')->constrained('categories')->cascadeOnDelete();
+
+        $table->string('title');
+        $table->text('description');
+        $table->timestamps();
+
+        $table->index(['user_id','cat_id']);
         });
     }
-
     /**
      * Reverse the migrations.
      */
