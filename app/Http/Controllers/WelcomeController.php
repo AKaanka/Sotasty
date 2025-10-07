0b73b037728164563b5c140d1babb0e5eb540d8f
AKaanka <kaan.kadioglu@code.berlin>
2025-10-07 14:35:13 +0200

    WelcomeController is modified

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,7 +10,7 @@ class WelcomeController extends Controller
     public function index()
     {
         // Fetch the latest 3 recipes from the database
-        $latestRecipes = Recipe::latest()->take(3)->get();
+        $latestRecipes = Recipe::latest()->take(4)->get();
 
         // Return the welcome view with the data
         return view('welcome', compact('latestRecipes'));
